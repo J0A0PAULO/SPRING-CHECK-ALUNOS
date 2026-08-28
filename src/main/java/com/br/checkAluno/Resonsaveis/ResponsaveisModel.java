b8166119813029c4415c023a0ba17ad7fb534176
João <moreirajoaopaulo951@gmail.com>
2026-08-28 16:50:25 -0300

    feat: Criado entidade da tabela responsaveis, classe Responsive model

---
+++ src/main/java/com/br/checkAluno/Resonsaveis/ResponsaveisModel.java
@@ -0,0 +1,46 @@
+package com.br.checkAluno.Resonsaveis;
+
+import com.br.checkAluno.Alunos.AlunosModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "responsaveis")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponsaveisModel {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(unique = true)
+    private Integer cpf;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String telefone;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    LocalDateTime localDateTime;
+
+    @OneToMany
+    @JoinColumn(name = "responsavel_id")
+    private List<AlunosModel> alunosModels;
+
+}
