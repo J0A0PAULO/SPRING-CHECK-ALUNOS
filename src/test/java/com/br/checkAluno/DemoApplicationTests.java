32577cff8c8b0a86fb7986591e309d85e9247c4e
João <moreirajoaopaulo951@gmail.com>
2026-08-28 12:47:36 -0300

    iniciando projeto

---
+++ src/test/java/com/br/checkAluno/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.checkAluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
