f3f6d5e9cf6ed4db2939d75518c7ebe71153c837
João <moreirajoaopaulo951@gmail.com>
2026-08-30 11:17:51 -0300

    feat: Criado Repository da entity Presenca

---
+++ src/main/java/com/br/checkAluno/Presencas/PresencaRepository.java
@@ -0,0 +1,6 @@
+package com.br.checkAluno.Presencas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PresencaRepository extends JpaRepository<PresencaModel, Long> {
+}
