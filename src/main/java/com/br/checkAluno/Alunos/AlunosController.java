f8471570350825afb1baa1228d0716d384cb5e0b
João <moreirajoaopaulo951@gmail.com>
2026-08-30 03:03:42 -0300

    feat: Criado rotas de Alunos na classe Alunos Controller

--- src/main/java/com/br/checkAluno/Alunos/AlunosController.java
+++ src/main/java/com/br/checkAluno/Alunos/AlunosController.java
@@ -1,4 +1,42 @@
 package com.br.checkAluno.Alunos;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("alunos")
 public class AlunosController {
+
+    AlunosService alunosService;
+
+
+    @GetMapping("/listar")
+    public List<AlunosModel> listarAlunos() {
+        return alunosService.listarAlunos();
+    }
+
+    @GetMapping("/listar/{id}")
+    public AlunosModel listarPorId(@PathVariable Long id) {
+        return alunosService.listarPorId(id);
+    }
+
+    @PostMapping("/criar")
+    public AlunosModel criarAluno(@RequestBody AlunosModel alunosModel) {
+        return alunosService.criarAluno(alunosModel);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletarAluno(@PathVariable Long id) {
+        alunosService.deletarAluno(id);
+    }
+
+    @PutMapping("/atualizar")
+    public AlunosModel atualizarAluno(@PathVariable Long id, @RequestBody AlunosModel alunosModel){
+        return alunosService.atualizarAluno(id,  alunosModel);
+    }
+
+
+
 }
