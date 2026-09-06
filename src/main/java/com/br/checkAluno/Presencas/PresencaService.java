85c6df117b10228e65e1debd8983ecfeebd364e0
João <moreirajoaopaulo951@gmail.com>
2026-09-06 00:20:03 -0300

    refact: Refatorado Presenca service para receber DTO

--- src/main/java/com/br/checkAluno/Presencas/PresencaService.java
+++ src/main/java/com/br/checkAluno/Presencas/PresencaService.java
@@ -1,51 +1,76 @@
 package com.br.checkAluno.Presencas;
 
+import com.br.checkAluno.Alunos.AlunosDTO;
+import com.br.checkAluno.Alunos.AlunosMapper;
 import com.br.checkAluno.Alunos.AlunosModel;
 import com.br.checkAluno.Alunos.AlunosRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PresencaService {
 
     PresencaRepository presencaRepository;
     AlunosRepository alunosRepository;
+    PresencaMapper presencaMapper;
+    AlunosMapper alunosMapper;
 
-    public PresencaService(PresencaRepository presencaRepository, AlunosRepository alunosRepository) {
+    public PresencaService(PresencaRepository presencaRepository, AlunosRepository alunosRepository, PresencaMapper presencaMapper, AlunosMapper alunosMapper) {
         this.presencaRepository = presencaRepository;
         this.alunosRepository = alunosRepository;
+        this.presencaMapper = presencaMapper;
+        this.alunosMapper = alunosMapper;
     }
 
-    public List<PresencaModel> listarPresenca() {
-        return presencaRepository.findAll();
+    public List<PresencaDTO> listarPresenca() {
+        List<PresencaModel> presencas = presencaRepository.findAll();
+        return presencas.stream().map(presencaMapper::map).collect(Collectors.toList());
     }
 
-    public PresencaModel listarPorId(Long id) {
+
+    public PresencaDTO listarPorId(Long id) {
         Optional<PresencaModel> encontradoPorID = presencaRepository.findById(id);
-        PresencaModel encontrado = encontradoPorID.orElse(null);
-        return encontrado;
-    }
-
-
-    public void deletar(Long id) {
-           presencaRepository.deleteById(id);
-    }
-
-    public PresencaModel criar(PresencaModel presencaModel) {
-        Optional<AlunosModel> alunoReferencia = alunosRepository.findById(presencaModel.getAlunosModel().getId());
-        AlunosModel alunoEncontrado = alunoReferencia.orElse(null);
-        presencaModel.setAlunosModel(alunoEncontrado);
-        return presencaRepository.save(presencaModel);
-    }
-
-    public PresencaModel atualizar(Long id, PresencaModel presencaModel) {
-        Optional<PresencaModel> PresensaPorID = presencaRepository.findById(id);
-        if (PresensaPorID.isPresent()) {
-            presencaModel.setId(id);
-           return presencaRepository.save(presencaModel);
+        if (encontradoPorID.isPresent()) {
+        PresencaModel presencaEncontrada = encontradoPorID.get();
+        PresencaDTO presencaDTO = presencaMapper.map(presencaEncontrada);
+        return presencaDTO;
         }
         return null;
+    }
+
+
+    public PresencaDTO criar(PresencaDTO presencaDTO) {
+        Optional<AlunosModel> alunoReferencia = alunosRepository.findById(presencaDTO.getAlunosModel().getId());
+        if (alunoReferencia.isPresent()){
+            AlunosModel aluno = alunoReferencia.get();
+            PresencaModel presencaModel = presencaMapper.map(presencaDTO);
+            presencaModel.setAlunosModel(aluno);
+            presencaRepository.save(presencaModel);
+            PresencaDTO presencaSalva = presencaMapper.map(presencaModel);
+            return presencaSalva;
+        }
+        return null;
+    }
+
+    public PresencaDTO atualizar(Long id, PresencaDTO presencaDTO) {
+
+        Optional<PresencaModel> PresensaPorID = presencaRepository.findById(id);
+        if (PresensaPorID.isPresent()) {
+        PresencaModel presencaEncontrada = PresensaPorID.get();
+
+        PresencaModel presecaConvertidoParaModel = presencaMapper.map(presencaDTO);
+        presecaConvertidoParaModel.setId(presencaEncontrada.getId());
+        presencaRepository.save(presecaConvertidoParaModel);
+        PresencaDTO presencaConvertidaParaDTO = presencaMapper.map(presecaConvertidoParaModel);
+        return presencaConvertidaParaDTO;
+        }
+        return null;
+    }
+
+    public void deletar(Long id) {
+        presencaRepository.deleteById(id);
     }
 }
