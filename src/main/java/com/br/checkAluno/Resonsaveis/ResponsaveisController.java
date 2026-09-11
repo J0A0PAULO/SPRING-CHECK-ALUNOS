9de14a42adad00fb51a9ba2d5c00ff925586ddde
João <moreirajoaopaulo951@gmail.com>
2026-09-10 22:31:09 -0300

    docs: criado documentação swagger para responsaveis

--- src/main/java/com/br/checkAluno/Resonsaveis/ResponsaveisController.java
+++ src/main/java/com/br/checkAluno/Resonsaveis/ResponsaveisController.java
@@ -1,12 +1,14 @@
 package com.br.checkAluno.Resonsaveis;
-
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Responsáveis", description = "Endpoints para gerenciamento de responsáveis")
 @RestController
 @RequestMapping("responsaveis")
 public class ResponsaveisController {
@@ -18,6 +20,9 @@ public class ResponsaveisController {
     }
 
     @Operation(summary = "Listar todos Responsaveis", description = "Essa rota retorna uma lista com todos os responsaveis dos alunos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listar todos responsaveis"),
+    })
     @GetMapping("/listar")
     public ResponseEntity<List<ResponsavelDTO>> listar() {
        List<ResponsavelDTO> responsavelDTOS = responsaveisService.listar();
@@ -25,6 +30,10 @@ public class ResponsaveisController {
     }
 
     @Operation(summary = "Listar responsavel por id", description = "Essa rota retorna um Responsavel por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Responsavel encontrado"),
+            @ApiResponse(responseCode = "404", description = "Responsavel não encontrado")
+    })
     @GetMapping("/listar/{id}")
     public ResponseEntity<?> listarPorId(@PathVariable Long id) {
       ResponsavelDTO responsavel = responsaveisService.listarPorId(id);
@@ -34,24 +43,40 @@ public class ResponsaveisController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Responsavel com " +  id + " Responsavle não encontrado");
     }
 
+    @Operation(summary = "Cria um responsavel", description = "Essa rota cria um responsavel")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Responsavel criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados do responsavel invalidos")
+    })
     @PostMapping("/criar")
     public ResponseEntity<?> criar(@RequestBody ResponsavelDTO responsavelDTO) {
-        responsaveisService.criar(responsavelDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Responsavel Criado");
+       ResponsavelDTO responsavel = responsaveisService.criar(responsavelDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responsavel);
     }
 
+    @Operation(summary = "Atualizar responsavel", description = "Essa rota atualiza responsavel")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Responsavel atualizado com  sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados do responsavel invalidos"),
+            @ApiResponse(responseCode = "404", description = "Responsavel não encontrado")
+    })
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<?> atualizar(@PathVariable Long id,@RequestBody ResponsavelDTO responsavelDTO) {
         ResponsavelDTO responsavel =  responsaveisService.atualizar(id  , responsavelDTO);
         if (responsavel != null) {
             return ResponseEntity.ok(responsavel);
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Responsavel com " + id + " Não encontrado");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responsavel);
     }
 
+    @Operation(summary = "Deletar responsavel", description = "Essa rota deleta responsavel")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Responsavel excluido com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Responsavel não encontrado")
+    })
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<?> deletar(@PathVariable Long id) {
          responsaveisService.deletar(id);
-         return ResponseEntity.ok("Responsavel Deletado");
+         return ResponseEntity.noContent().build();
     }
 }
