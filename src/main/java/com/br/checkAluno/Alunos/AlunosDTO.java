8d4d357c364570123bd9b0497708b353bbfc389b
João <moreirajoaopaulo951@gmail.com>
2026-09-13 22:25:49 -0300

    feat: Adiciona spring validation na camada alunosDTO

--- src/main/java/com/br/checkAluno/Alunos/AlunosDTO.java
+++ src/main/java/com/br/checkAluno/Alunos/AlunosDTO.java
@@ -3,9 +3,14 @@ package com.br.checkAluno.Alunos;
 import com.br.checkAluno.Responsaveis.ResponsaveisModel;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
@@ -15,13 +20,28 @@ import java.time.LocalDate;
 public class AlunosDTO {
 
     private Long id;
+
+    @NotBlank(message = "Nome é obrigatorio")
     private String nome;
+
+    @NotBlank(message = "CPF é obrigatorio")
+    @CPF(message = "CPF invalido")
     private String cpf;
+
+    @NotBlank(message = "Email é obrigatorio")
+    @Email(message = "Email invalido")
     private String email;
+
+    @NotBlank(message = "Telefone é obrigatorio")
     private String telefone;
+
+    @NotNull(message = "Matricula é obrigatoria")
     private Long matricula;
+
     private LocalDate dataNascimento;
+
     @JsonIgnoreProperties("aluno")
     @Schema(hidden = true)
+    @Valid
     private ResponsaveisModel responsavel;
 }
